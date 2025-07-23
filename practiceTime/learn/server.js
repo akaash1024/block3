25242d053d0a49711c2d0de2cd6e9f3f644be6de
Akash <akashkewat99@gmail.com>
2025-07-23 14:20:11 +0530

    updated

--- practiceTime/learn/server.js
+++ practiceTime/learn/server.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 
-const express = require("express");
-const path = require("path")
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8000;
 
+const express = require("express");
+const connectDB = require("./connectDB");
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json())
 
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening at port ${PORT}`);
 
-app.listen(PORT, ()=>{
-    console.log(blueBright(`Server is running at http://localhost:${PORT}`));
+    })
 })
+
